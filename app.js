8311e995806a1f545d4be1d6092db67bb4e62be1
Daniel Sullivan <ojojoj89@gmail.com>
2023-10-02 19:00:04 -0500

    began working on project, set up the html, and began working on the css

---
+++ app.js
@@ -0,0 +1,6 @@
+var day1Forecast = document.querySelector('#forecast1');
+var day2Forecast = document.querySelector('#forecast2');
+var day3Forecast = document.querySelector('#forecast3');
+var day4Forecast = document.querySelector('#forecast4');
+var day5Forecast = document.querySelector('#forecast5');
+var todaysForecast = document.queryselector('.weatherContentToday');
