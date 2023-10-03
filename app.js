eb989ff1083c653e5d7b5b5c61136653ebf6e0b3
Daniel Sullivan <ojojoj89@gmail.com>
2023-10-02 22:05:58 -0500

    added comments to code

--- app.js
+++ app.js
@@ -10,56 +10,70 @@ var windSpeed = document.querySelector('#cityWindSpeed');
 var cityName = document.querySelector('#cityName');
 var searchButton = document.querySelector('.searchButton');
 var appid = 'd665717576f6934ee876304b7f3d052b';
-function getParams(){
-    var searchParamsArr = document.location.search.split('&');
-    var query = searchParamsArr[0].split('=').pop();
-    var appid = searchParamsArr[2].split('=').pop();
-    searchApi(query,appid);
-}
+// when event listener is triggered, the function will run
 function searchForecast(event) {
     event.preventDefault();
+    // location will be pulled from the input value
     var location = document.querySelector('#location').value;
     
-
+    // if the location is empty, the function will display an error message then return
     if (!location) {
         console.error('You need to enter a city name');
         return;
     }
-
-    var queryString = '/geo/1.0/direct?q=' + searchInputVal + '&limit=5&appid=' + appid;
+// setting the query string to the api url
+    var queryString = '/geo/1.0/direct?q=' + location + '&limit=5&appid=' + appid;
         location.assign(queryString);
         cityName = location;
+        //calling searchApi function
         (searchApi);
 }
 
 function searchApi(query, appid) {
+    // setting the query string to the api url
     var locQueryUrl = 'https://api.openweathermap.org/';
     locQueryUrl = locQueryUrl + 'geo/1.0/direct?q=' + query + '&limit=5&appid=' + appid;
+    //fetching the api longitute and latitude
     var lon = locQueryUrl.lon;
     var lat = locQueryUrl.lat;
+    //setting the local storage to the longitute and latitude
     lon = JSON.parse(localStorage.getItem('lon')) || [];
     lat = JSON.parse(localStorage.getItem('lat')) || [];
     localStorage.setItem('lon', 'lat', JSON.stringify(lon, lat));
+    //triggering second search function
+        //checking for response from webpage
+fetch(locQueryUrl)
+.then(function (response) {
+    if (!response.ok) {
+        throw response.json();
+ }
+    return response.json();
+})
+}
     secondSearch();
 
-function secondSearch(lon, lat, query, appid) {
+function secondSearch(lon, lat,  location, appid) {
+    //setting the query string to the api url
     var forecastUrl = 'https://api.openweathermap.org/';
-    forecastUrl = forecastUrl + 'data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + appid;
+    forecastUrl = forecastUrl + 'data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + appid;\
+    //fetching the api data
     maxTemperature = [];
     humidity = [];
     windSpeed = [];
     maxTemperature = forecastUrl.list[0,1,2,3,4].main.temp_max;
     humidity = forecastUrl.list[0,1,2,3,4].main.humidity;
     windSpeed = forecastUrl[0,1,2,3,4].imperial.wind.speed;
-    day1Forecast = forecastUrl.list[0].weather[0].icon + "Max Temperature: " + maxTemperature[1] + "\nHumidity: " + humidity[1] + "\nWind Speed: " + windSpeed[1];
-    day2Forecast = forecastUrl.list[1].weather[0].icon + "Max Temperature: " + maxTemperature[2] + "\nHumidity: " + humidity[2] + "\nWind Speed: " + windSpeed[2];
-    day3Forecast = forecastUrl.list[2].weather[0].icon + "Max Temperature: " + maxTemperature[3] + "\nHumidity: " + humidity[3] + "\nWind Speed: " + windSpeed[3];
-    day4Forecast = forecastUrl.list[3].weather[0].icon + "Max Temperature: " + maxTemperature[4] + "\nHumidity: " + humidity[4] + "\nWind Speed: " + windSpeed[4];
-    day5Forecast = forecastUrl.list[4].weather[0].icon + "Max Temperature: " + maxTemperature[5] + "\nHumidity: " + humidity[5] + "\nWind Speed: " + windSpeed[5];
+    //setting the display and local storage from the api data
+    day1Forecast = forecastUrl.list[0].weather[0].icon + "Location: " + location + "\nMax Temperature: " + maxTemperature[1] + "\nHumidity: " + humidity[1] + "\nWind Speed: " + windSpeed[1];
+    day2Forecast = forecastUrl.list[1].weather[0].icon + "Location: " + location + "\nMax Temperature: " + maxTemperature[2] + "\nHumidity: " + humidity[2] + "\nWind Speed: " + windSpeed[2];
+    day3Forecast = forecastUrl.list[2].weather[0].icon + "Location: " + location + "\nMax Temperature: " + maxTemperature[3] + "\nHumidity: " + humidity[3] + "\nWind Speed: " + windSpeed[3];
+    day4Forecast = forecastUrl.list[3].weather[0].icon + "Location: " + location + "\nMax Temperature: " + maxTemperature[4] + "\nHumidity: " + humidity[4] + "\nWind Speed: " + windSpeed[4];
+    day5Forecast = forecastUrl.list[4].weather[0].icon + "Location: " + location + "\nMax Temperature: " + maxTemperature[5] + "\nHumidity: " + humidity[5] + "\nWind Speed: " + windSpeed[5];
     maxTemperature = JSON.parse(localStorage.getItem('maxTemperature')) || [];
     humidity = JSON.parse(localStorage.getItem('humidity')) || [];
     windSpeed = JSON.parse(localStorage.getItem('windSpeed')) || [];
     }
+    //checking for response from webpage
 fetch(locQueryUrl)
     .then(function (response) {
         if (!response.ok) {
@@ -67,6 +81,5 @@ fetch(locQueryUrl)
      }
         return response.json();
     })
-}
-getParams();
+//event listener for the search button
 searchButton.onclick = searchForecast;
